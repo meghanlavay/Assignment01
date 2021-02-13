7c9de64439642313ad7f7325af3eea36e93d4230
meghanlavay <77410138+meghanlavay@users.noreply.github.com>
2021-02-12 19:18:33 -0800

    init

---
+++ js/script.js
@@ -0,0 +1,188 @@
+// STEP 1
+/*
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let MyLibraryFunction
+*/
+
+// STEP 2
+/*
+5   // numeric literal expression 
+"welcome"   // string literal expression
+true    // Boolean literal expression
+null    // null literal expression
+*/
+
+// STEP 3
+/*
+let anExpression = 5 + (2 / 3) * 3;
+let aSecondExpression = (price1 + price2) / 2;
+*/
+
+// STEP 4
+/*
+let sFirstName;
+let sLastName;
+let sAddress;
+let sCity;
+let sState;
+let sZipCode;
+let iMyAge;
+let sReferralSource;
+let bMayWeContactYou;
+*/
+
+// STEP 5
+/*
+let sFirstName;
+let bMayWeContactYou;
+sFirstName = "Meghan";
+bMayWeContactYou - true;
+
+let sLastName ="LaVay";
+let bMayWeContactYou = true;
+
+let sCity = "San Diego", bMayWeContactYou = true;
+*/
+
+// STEP 6
+/*
+let classesCompleted = 3;
+window.console.log("Meghan has completed " + classesCompleted + " classes.");
+*/
+
+// STEP 7
+/*
+let isThereClassOnThursday = true;
+window.console.log("It is " + isThereClassOnThursday + " that we have class on Thursday." );
+*/
+
+/*
+let theNumberWillEqualFive = true;
+window.console.log(4+ theNumberWillEqualFive);
+*/
+
+// STEP 8
+/*
+let someString = "Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure abput the former.\" ";
+window.console.log(someString);
+*/
+
+// STEP 9
+/*
+let x = null;
+window.console.log(x);
+*/
+
+/*
+let x;
+window.console.log(x);
+*/
+
+// STEP 10
+//string
+/*
+let num = "Hello!";
+window.console.log(typeof num);
+*/
+
+//number
+/*
+let num = 5;
+window.console.log(typeof num);
+*/
+
+//boolean
+/*
+let num = true;
+window.console.log(typeof num);
+*/
+
+//object
+/*
+let num = new Object();
+window.console.log(typeof num);
+*/
+
+//undefined
+/*
+let x;
+window.console.log(typeof x);
+*/
+
+// STEP 11
+/*
+alert("Hello "+ "Meghan LaVay" + ", welcome to the JavaScript class!");
+*/
+
+// STEP 12
+/*let name = "Meghan LaVay";
+alert("Hello " + name + ", welcome to the JavaScript class!");
+*/
+
+// STEP 13
+/*
+let name = "Meghan LaVay";
+let course = "JavaScript";
+alert("Hello " + name + ", welcome to the " + course + " class!");
+*/
+
+// STEP 14
+/*
+let name = "Meghan LaVay";
+let course = "JavaScript";
+alert("Hello " + name + ", \nWelcome to the " + course + " class!");
+*/
+
+// STEP 15
+/*
+let name = prompt ("What is your name?");
+let course = "JavaScript";
+alert("Hello " + name + ", \nWelcome to the " + course + " class!");
+*/
+
+// STEP 16
+/*
+let name = prompt ("What is your name?");
+let course = prompt ("What class are you taking?");
+alert("Hello " + name + ", \nWelcome to the " + course + " class!");
+*/
+
+// STEP 17
+/*
+let x = 10;
+let y =20;
+window.console.log(x + y);
+*/
+
+// STEP 18
+/*
+let x = 20;
+window.console.log(x += 20);
+*/
+
+// STEP 19
+/*
+let x = 20;
+window.console.log(x *= 5);
+*/
+
+// STEP 20
+// let x = 20 % 3;
+// window.console.log(x /= 1);
+
+// STEP 21
+/*
+let x = 3
+let y = 5
+window.console.log(x != 2 || y != 14);
+*/
+
+// STEP 22
+/*
+let x = 10;
+let y = 20;
+window.console.log(x == 2 && y == 14);
+*/
